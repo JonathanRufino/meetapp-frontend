87b4283a3bd1382a95e96dd91cf83300f8f7c77c
Jonathan Rufino <jonathanrufinopaiva@gmail.com>
2019-07-28 10:33:28 -0300

    chore: add forms validation

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+const SIGN_IN_SCHEMA = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
 
 function SignIn() {
   function handleSubmit(data) {
@@ -13,7 +21,7 @@ function SignIn() {
     <>
       <img src={logo} alt="Meetapp" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={SIGN_IN_SCHEMA} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Digite seu e-mail" />
         <Input
           name="password"
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+const SIGN_UP_SCHEMA = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'No mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
 
 function SignUp() {
   function handleSubmit(data) {
@@ -13,7 +24,7 @@ function SignUp() {
     <>
       <img src={logo} alt="Meetapp" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={SIGN_UP_SCHEMA} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Digite seu e-mail" />
         <Input
