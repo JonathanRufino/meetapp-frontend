8acb692d386154472846d5746b0d408f8a016161
Jonathan Rufino <jonathanrufinopaiva@gmail.com>
2019-07-29 21:20:12 -0300

    chore: display user info on header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import logo from '~/assets/logo.svg';
 
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -18,7 +21,7 @@ export default function Header() {
         <aside>
           <Profile>
             <div>
-              <strong>Jonathan Rufino</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
           </Profile>
