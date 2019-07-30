1311be3a43283ab0df759755271c34cb530b00fe
Jonathan Rufino <jonathanrufinopaiva@gmail.com>
2019-07-29 21:29:15 -0300

    feature: add logout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import logo from '~/assets/logo.svg';
+import { signOut } from '~/store/modules/auth/actions';
 
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const dispatch = useDispatch();
+
   const profile = useSelector(state => state.user.profile);
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
 
   return (
     <Container>
@@ -26,7 +33,9 @@ export default function Header() {
             </div>
           </Profile>
 
-          <button type="button">Sair</button>
+          <button type="button" onClick={handleSignOut}>
+            Sair
+          </button>
         </aside>
       </Content>
     </Container>
