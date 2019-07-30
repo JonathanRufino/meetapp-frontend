e5e2a58a90baf4ff0dc22f2da96c5463f4380afd
Jonathan Rufino <jonathanrufinopaiva@gmail.com>
2019-07-29 21:55:33 -0300

    new: create dashboard page structure

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Meetup } from './styles';
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <h1>Meus meetups</h1>
+
+        <button type="button">Novo meetup</button>
+      </header>
+
+      <ul>
+        <Meetup>
+          <strong>Meetup de React Native</strong>
+          <span>24 de Junho, às 20h</span>
+        </Meetup>
+        <Meetup>
+          <strong>NodeJS Meetup</strong>
+          <span>17 de Julho, às 13h</span>
+        </Meetup>
+        <Meetup>
+          <strong>Rocketseat Meetup</strong>
+          <span>30 de Agosto, às 20h</span>
+        </Meetup>
+        <Meetup>
+          <strong>React on the house!</strong>
+          <span>17 de Novembro, às 16h</span>
+        </Meetup>
+      </ul>
+    </Container>
+  );
 }
