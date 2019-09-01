c3c5f878e14616ad0013705cb10350541788ce82
Jonathan Rufino <jonathanrufinopaiva@gmail.com>
2019-09-01 18:28:42 -0300

    chore: add locale header to requests

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,18 @@
 import axios from 'axios';
 
+import i18n from '~/i18n';
+
 const api = axios.create({
   baseURL: 'http://localhost:3333',
 });
+
+api.interceptors.request.use(
+  config => {
+    config.headers['Accept-Language'] = i18n.language;
+
+    return config;
+  },
+  err => Promise.reject(err)
+);
 
 export default api;
