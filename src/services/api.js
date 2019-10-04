ae915274bf8aa8628b7281d178bb3c5c2bbd20ee
Jonathan Rufino <jonathanrufinopaiva@gmail.com>
2019-10-04 09:46:55 -0300

    chore: add fake api

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,13 @@ import axios from 'axios';
 
 import i18n from '~/config/i18n';
 
+const baseURL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3333'
+    : 'https://my-json-server.typicode.com/JonathanRufino/meetapp-frontend';
+
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL,
 });
 
 api.interceptors.request.use(
